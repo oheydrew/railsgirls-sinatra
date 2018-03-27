9e2c8d258f505f3d10473e230da2c70a45ead241
oheydrew <drew@oheydrew.me>
2018-03-27 17:50:23 +1100

    Add YAML storage, Modify logic + templates

--- suffragist.rb
+++ suffragist.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 CHOICES = {
   'HAM' => 'Hamburger',
@@ -7,14 +8,38 @@ CHOICES = {
   'NOO' => 'Noodles',
 }
 
+configure do
+  set :port, 3000
+end
+
 # Route for / - home directory
 get '/' do
   @title = 'Welcome to the Suffragist!' # Set @title var for view
   erb :index # run ERB on index.erb
 end
 
+# Route for /cast post
 post '/cast' do
   @title = 'Thanks for casting your vote!'
   @vote  = params['vote']
+
+  # YAML based storage
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  
   erb :cast
 end
+
+# Route for /results page
+get '/results' do
+  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
+
+
